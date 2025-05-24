4f1e564c14451bc0306ef889bfe3d7dbae25c322
upasana1927 <145797131+upasana1927@users.noreply.github.com>
2025-05-24 20:56:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import Login from "./Login";
 import RegisterPage from "./RegisterPage";
 import HomePage from "./Home";
 import InboxPage from "./InboxPage";
+import MessageView from "./MessageView";
 import ProtectedRoute from "./ProtectedRoute";
+import SendMessage from "./SendMessage";
 
 function App() {
   return (
@@ -13,8 +15,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+        <Route path="/send" element={<ProtectedRoute><SendMessage /></ProtectedRoute>} />
         <Route path="/inbox" element={<ProtectedRoute><InboxPage /></ProtectedRoute>} />
-        
+        <Route path="/inbox/:uid" element={<ProtectedRoute><MessageView /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
